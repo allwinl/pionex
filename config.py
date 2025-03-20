bf9a5130995b94d71d957fd2943873b27424aa4d
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2025-03-20 20:31:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # Pionex API credentials
 API_KEY = "6P2BU19dXFdN8SjLgbSnvbECny5HbT8nGXKtQf5yDeYB6fq3yGeeZ1CzafRhMnm3L5"
 SECRET_KEY = "MlGRRZYxIXPxatJEYv1afhpFgGQAfUAH76LAgzvKutEa2tqRtjnXXbGDLuViuT5H" 
+
+TELEGRAM_TOKEN_RSI = "your_telegram_bot_token"
+CHAT_ID = "your_telegram_chat_id"
